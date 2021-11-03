eb338d7370cb14791fd7d7daee731d76f735daa0
Prince-Thind <princethind799@gmail.com>
2021-11-03 23:32:57 +0530

    add bcryptjs and express validator package

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,16 @@
+const {body,validatonResult}=require('express-validator')
 
 exports.user_create_get = function (req, res, next) {
-    res.send('Not Implemented: user create get');
-  };
-  
-  exports.user_create_post = function (req, res, next) {
-    res.send('Not Implemented: user create post');
-  };
-  
-  exports.user_login_get = function (req, res, next) {
-    res.send('Not Implemented: user login get');
-  };
-  exports.user_login_post = function (req, res, next) {
-    res.send('Not Implemented: user login post');
-  };
-  
+  res.render('sign_in',{title:'Sign In'})
+};
+
+exports.user_create_post = function (req, res, next) {
+  res.send('Not Implemented: user create post');
+};
+
+exports.user_login_get = function (req, res, next) {
+  res.send('Not Implemented: user login get');
+};
+exports.user_login_post = function (req, res, next) {
+  res.send('Not Implemented: user login post');
+};
