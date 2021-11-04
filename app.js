062bf10f2594e864676848358c10113c3d57444d
Prince-Thind <princethind799@gmail.com>
2021-11-04 15:55:32 +0530

    set up production environment

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const helmet=require('helmet');
+const compression=require('compression')
+
 //auth requires
 const session = require('express-session');
 const passport = require('passport');
@@ -31,6 +34,8 @@ db.on('error', console.error.bind(console, 'MongoDB Connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(compression());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
